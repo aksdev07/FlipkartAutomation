d94365148b63551b52a261448601f4aedf0f92ac
aktester07 <47987593+aktester07@users.noreply.github.com>
2019-10-08 13:11:26 +0530

    added item search and item click functionality

--- src/main/java/TestCases.java
+++ src/main/java/TestCases.java
@@ -6,7 +6,7 @@ abstract public class TestCases {
 
     protected  abstract void searchItemFunctionality() throws InterruptedException;
 
-    protected abstract void itemSelectionFunctionality() throws InterruptedException;
+    protected abstract void itemSelectionFunctionality() throws InterruptedException, IOException;
 
 
     protected abstract void purchaseFunctionality();
